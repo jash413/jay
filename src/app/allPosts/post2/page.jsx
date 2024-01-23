910977b61dbc03f7d269616e385df888d41c78ac
utkarshyadav11 <utkarshyadav6745@gmail.com>
2024-01-23 15:34:34 +0530

    max flow

--- src/app/allPosts/post2/page.jsx
+++ src/app/allPosts/post2/page.jsx
@@ -1,16 +1,95 @@
-import React from 'react'
+"use client";
+import React, { useEffect, useState } from "react";
+import Navbar from "@/components/navbar/index";
+import { AnimatePresence } from "framer-motion";
+import Preloader from "@/components/preloader/page";
 import InnerBlogPost from "@/common/innerBlogPost/page"
 import BlogPostImage2 from "@/images/blogs_img2.png";
 const Page = () => {
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      const LocomotiveScroll = (await import("locomotive-scroll")).default;
+      const locomotiveScroll = new LocomotiveScroll();
+    })();
+  }, []);
+
+  setTimeout(() => {
+    setIsLoading(false);
+    if (typeof document !== "undefined") {
+      document.body.style.cursor = "default";
+      window.scrollTo(0, 0);
+    }
+  }, 2000);
   return (
     <div>
+
+<AnimatePresence mode="wait">
+        {isLoading && <Preloader />}
+      </AnimatePresence>
+
+      <Navbar />
       <InnerBlogPost
 
 image={BlogPostImage2}
         Date="March 20, 2024"
         blog_name="Interior Design"
-        blogPost_heading="Types of plywood size & what are the difference"
-        blogPost_text="In the rich tapestry of Indian culture, the pooja room holds a unique and revered place. It’s not merely a physical space within a home; it’s a sanctuary where families come together to connect with their spirituality, find solace, and offer their devotion. Designing a pooja room is not just a matter of aesthetics; it’s about crafting an environment that…"
+        blogPost_heading="5 Tips on how to maintain plywood sheets"
+        blogPost_text="Plywood, a versatile and widely used material in construction and woodworking, requires proper maintenance to ensure its longevity and performance. Whether you're working with plywood sheets for furniture, cabinetry, or structural applications, implementing effective maintenance practices is crucial. In this comprehensive guide, we'll explore five valuable tips on how to maintain plywood sheets, preserving their integrity and aesthetics"
+
+        blogPost_heading1="1. Shield Plywood from Moisture"
+        blogPost_text1="Plywood sheets are susceptible to moisture damage, which can lead to warping, swelling, and even fungal growth. To protect your plywood, avoid exposing it to excessive moisture or damp conditions. In outdoor applications, ensure proper sealing with waterproof coatings or paints to create a barrier against rain and humidity. Indoors, maintain a dry environment and address leaks promptly to prevent water damage."
+       
+
+         texture7="Tip: Apply Waterproof Coatings"
+         text7="Consider applying waterproof coatings or sealants to plywood surfaces, especially in areas prone to moisture exposure. This extra layer of protection enhances the plywood's resistance to water and extends its lifespan."
+
+         blogPost_heading2="2. Provide Adequate Ventilation"
+        blogPost_text2="Proper ventilation is essential to prevent the buildup of humidity, which can adversely affect plywood sheets. In enclosed spaces such as cabinets or furniture, ensure adequate airflow to reduce the risk of moisture retention. This is particularly important in areas with high humidity levels, such as kitchens and bathrooms, where ventilation can help maintain the structural integrity of the plywood."
+
+        texture6="Tip: Use Ventilation Devices"
+         text6="In spaces with limited natural ventilation, consider using fans or dehumidifiers to regulate moisture levels. These devices can be instrumental in creating a dry and well-ventilated environment for your plywood."
+
+
+         blogPost_heading3="3. Avoid Direct Sunlight Exposure"
+        blogPost_text3="Prolonged exposure to direct sunlight can lead to discoloration and degradation of plywood. If plywood sheets are used in outdoor applications, such as decks or exterior cladding, consider incorporating shading elements to protect them from harsh sunlight. Indoors, position furniture or cabinets away from direct sunlight to preserve the natural appearance of the plywood."
+
+
+        texture8="Tip: Apply UV-Resistant Finishes"
+         text8="When finishing plywood surfaces, choose UV-resistant coatings or finishes. These products provide an added layer of protection against the damaging effects of sunlight, ensuring that your plywood retains its original color and texture over time."
+
+
+
+         blogPost_heading4="4. Regular Cleaning Routine"
+        blogPost_text4="Maintaining the cleanliness of plywood sheets is essential for both aesthetic and functional reasons. Dust and debris can accumulate on surfaces, affecting the appearance and potentially leading to scratches or abrasions. Implementing a regular cleaning routine is a simple yet effective way to preserve the visual appeal of plywood."
+
+        texture9="Tip: How to clean plywood?"
+         text9="Use a mild soap and water solution for routine cleaning. Dampen a soft cloth in the solution and gently wipe down the plywood surfaces. Avoid using abrasive cleaners or harsh chemicals, as these can damage the finish and compromise the plywood's natural appearance."
+
+         blogPost_heading5="5. Protect Plywood Edges and Corners"
+        blogPost_text5="The edges and corners of plywood sheets are vulnerable to damage, especially in high-traffic areas or during transportation. To prevent chipping or splintering, take extra precautions to protect these areas. Employing edge banding, applying sealants, or using protective caps can safeguard plywood edges from wear and tear."
+
+        texture10="Tip: Reinforce Edges with Sealants"
+         text10="Apply edge sealants or finishes to reinforce the vulnerable edges of plywood. This not only adds a protective layer but also enhances the overall durability of the plywood, especially in applications where edges are more prone to impact."
+
+
+         
+       
+
+         blogPost_heading8="Conclusion"
+        blogPost_text8="Maintaining the excellence of plywood sheets is not only a matter of aesthetics but also a crucial step in ensuring their structural integrity and performance. By implementing these five tips on how to maintain plywood sheets, you can extend their lifespan, preserve their natural beauty, and enhance their resistance to environmental factors"
+        blogPost_text8a="From shielding plywood from moisture to providing proper ventilation, each tip contributes to a holistic approach to plywood maintenance. Regular cleaning routines and protective measures for edges and corners further reinforce the durability of plywood sheets, making them a reliable and enduring choice for a wide range of applications. Incorporate these tips into your plywood care routine, and enjoy the long-lasting benefits of well-maintained plywood in your projects."
+
+
+
+        // texture3="Tip: Use Ventilation Devices"
+        //  text3="In spaces with limited natural ventilation, consider using fans or dehumidifiers to regulate moisture levels. These devices can be instrumental in creating a dry and well-ventilated environment for your plywood."
+
+
+        //  blogPost_heading3="3. Avoid Direct Sunlight Exposure"
+        // blogPost_text3="Prolonged exposure to direct sunlight can lead to discoloration and degradation of plywood. If plywood sheets are used in outdoor applications, such as decks or exterior cladding, consider incorporating shading elements to protect them from harsh sunlight. Indoors, position furniture or cabinets away from direct sunlight to preserve the natural appearance of the plywood."
 
 
       />
--- src/common/blogPosts/data.jsx
+++ src/common/blogPosts/data.jsx
@@ -9,7 +9,7 @@ const Data = [
         date: "January 11, 2024",
         blog_name: "Interior Design1",
         blogPost_heading: "5 Advantages of choosing waterproof plywood",
-        blogPost_text: "In the rich tapestry of Indian culture, the pooja room holds a unique and revered place. It’s not merely a physical space within a home; it’s a sanctuary where families come together to connect with their spirituality, find solace, and offer their devotion. Designing a pooja room is not just a matter of aesthetics; it’s about crafting an environment that…",
+        blogPost_text: "IIn the realm of construction and woodworking, the choice of materials plays a pivotal role in determining the longevity and quality of the end product. Waterproof plywood stands out as a game-changer, offering a range of advantages that make it a preferred choice for various applications. ",
         readMoreRoute: "/allPosts/post1",
     },
     {
@@ -17,7 +17,7 @@ const Data = [
         date: "March 20, 2024",
         blog_name: "Interior Design2",
         blogPost_heading: "5 Tips on how to maintain plywood sheets",
-        blogPost_text: "In the rich tapestry of Indian culture, the pooja room holds a unique and revered place. It’s not merely a physical space within a home; it’s a sanctuary where families come together to connect with their spirituality, find solace, and offer their devotion. Designing a pooja room is not just a matter of aesthetics; it’s about crafting an environment that…",
+        blogPost_text: "Plywood, a versatile and widely used material in construction and woodworking, requires proper maintenance to ensure its longevity and performance. Whether you're working with plywood sheets for furniture, cabinetry, or structural applications, implementing effective maintenance practices is crucial. ",
         readMoreRoute: "/allPosts/post2",
     },
     {
@@ -25,7 +25,7 @@ const Data = [
         date: "March 20, 2024",
         blog_name: "Interior Design3",
         blogPost_heading: "7 Easy ways to clean plywood furniture",
-        blogPost_text: "In the rich tapestry of Indian culture, the pooja room holds a unique and revered place. It’s not merely a physical space within a home; it’s a sanctuary where families come together to connect with their spirituality, find solace, and offer their devotion. Designing a pooja room is not just a matter of aesthetics; it’s about crafting an environment that…",
+        blogPost_text: "Plywood furniture is not only trendy but also known for its durability and affordability. To ensure your plywood furniture retains its aesthetic appeal and longevity, proper cleaning and maintenance are crucial. Let’s explore seven easy ways to clean plywood furniture, covering the best quality plywood options and tips on maintaining plyboard furniture.",
         readMoreRoute: "/allPosts/post3",
     },
 
@@ -33,8 +33,8 @@ const Data = [
         image: BlogPostImage1,
         date: "January 11, 2024",
         blog_name: "Interior Design4",
-        blogPost_heading: "5 Advantages of choosing waterproof plywood",
-        blogPost_text: "In the rich tapestry of Indian culture, the pooja room holds a unique and revered place. It’s not merely a physical space within a home; it’s a sanctuary where families come together to connect with their spirituality, find solace, and offer their devotion. Designing a pooja room is not just a matter of aesthetics; it’s about crafting an environment that…",
+        blogPost_heading: "Types of plywood size & what are the difference",
+        blogPost_text: "Plywood, a versatile and widely used material in construction and woodworking, comes in various sizes, with 8x4 being a common dimension. Among the choices available, 18mm and 12mm plywood stand out as popular options. Let’s explore the types of plywood sizes, focusing on the differences between 18mm plywood and 12mm plywood, along with insights into pricing for both.",
         readMoreRoute: "/allPosts/post4",
     },
     {
