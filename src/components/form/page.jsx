8fc4859cee593867d95d47e31b48a599afd06204
utkarshyadav11 <utkarshyadav6745@gmail.com>
2024-01-29 19:39:06 +0530

    meet

--- src/components/form/page.jsx
+++ src/components/form/page.jsx
@@ -8,7 +8,7 @@ import Image from "next/image";
 import { Slide, ToastContainer, toast } from "react-toastify";
 import emailjs from "@emailjs/browser";
 import "react-toastify/dist/ReactToastify.css";
-import form_image from "@/images/formNewImage.png";
+import form_image from "@/images/formNewImage.jpg";
 import { ContactFormSchemas } from "@/components/schemas/page";
 import "./form.css";
 const Contactform = () => {
