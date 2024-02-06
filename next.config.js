3cb33ed4e9681b7eda26b7d4784d9182167c7339
utkarshyadav11 <utkarshyadav6745@gmail.com>
2024-02-06 19:29:34 +0530

    just

--- next.config.js
+++ next.config.js
@@ -11,12 +11,12 @@ module.exports = nextConfig
 
 
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-      enabled: process.env.ANALYZE === 'true'
-  })
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//       enabled: process.env.ANALYZE === 'true'
+//   })
     
-  module.exports = withBundleAnalyzer({
-      env: {
-          NEXT_PUBLIC_ENV: 'PRODUCTION', //your next configs goes here
-      },
-  })
+//   module.exports = withBundleAnalyzer({
+//       env: {
+//           NEXT_PUBLIC_ENV: 'PRODUCTION', //your next configs goes here
+//       },
+//   })
