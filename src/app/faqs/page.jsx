a48deffd8ffe63cac8f3d6a3d70e6729e3d3f440
utkarshyadav11 <utkarshyadav6745@gmail.com>
2024-02-09 14:28:05 +0530

    number

--- src/app/faqs/page.jsx
+++ src/app/faqs/page.jsx
@@ -5,10 +5,7 @@ import { AnimatePresence } from "framer-motion"
 import Inner_header from "@/common/inner_header/page"
 import Preloader from "@/components/preloader/page"
 import Claim_banner from "@/images/claim_banner.png"
-// import Faqs from "@/components/faqs/page"
 import Faqs_page from "@/components/faqs_page/page"
-// import Faqs  from "@/components/faqs/page";
-// import Navbar from "@/components/navbar/index"
 const Page = () => {
   const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
