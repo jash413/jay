e3f6692b7242ff9309a38b12ac434673a032bb7d
meetchauhandweck <meet.chauhan@dweckenterprise.com>
2024-02-13 16:57:32 +0530

    url changes -m

--- src/components/footer/page.jsx
+++ src/components/footer/page.jsx
@@ -10,11 +10,13 @@ import Button from "@/common/button/button4";
 import ScrollToTop from "react-scroll-to-top";
 import Title from "@/components/footer/test";
 import styles from "@/components/footer/footer.module.css";
-import "./scroll_to_top.css"
+import "./scroll_to_top.css";
+import { usePathname } from "next/navigation";
 const Page = () => {
   const [ref, inView] = useInView({
     triggerOnce: true,
   });
+  const location = usePathname();
 
   const controls = useAnimation();
 
@@ -44,10 +46,11 @@ const Page = () => {
             className={styles.footer_upper_heading}
           />
         </motion.div>
-
-        <div className={styles.justforfooterbutton}>
-          <Button btn_text="Contact Now" />
-        </div>
+        {location != "/contactUs" && (
+          <div className={styles.justforfooterbutton}>
+            <Button btn_text="Contact Now" />
+          </div>
+        )}
       </div>
       <Title />
       <div className={styles.footer_wrapper}>
@@ -77,9 +80,9 @@ const Page = () => {
           <div className={styles.footer_titles}>{"HELP"}</div>
           <ul className={styles.footer_headers}>
             <li className={styles.footer_item_list}>
-            <Link href="/privacyPolicy">{"Privacy policy"}</Link>
+              <Link href="/privacyPolicy">{"Privacy policy"}</Link>
             </li>
-            
+
             <li className={styles.footer_item_list}>
               <Link href="/claimWarranty">{"Claim warranty"}</Link>
             </li>
@@ -87,8 +90,8 @@ const Page = () => {
               <Link href="/contactUs">{"Contact us"}</Link>
             </li>
             <li className={styles.footer_item_list}>
-              <Link href="/faqs">{"FAQ's"}</Link> 
-              </li>
+              <Link href="/faqs">{"FAQ's"}</Link>
+            </li>
           </ul>
         </div>
 
@@ -96,11 +99,14 @@ const Page = () => {
           <div className={styles.footer_titles}>{"PRODUCT GUIDE"}</div>
           <ul className={styles.footer_headers}>
             <li className={styles.footer_item_list}>
-             <Link href="https://royaletouche.com/" >{"Laminates"}</Link>
-              </li>
+              <Link href="https://royaletouche.com/">{"Laminates"}</Link>
+            </li>
             <li className={styles.footer_item_list}>
-              <Link href="https://royaletouche.com/woodenfloors/"> {"Wooden Floors"} </Link>
-              </li>
+              <Link href="https://royaletouche.com/woodenfloors/">
+                {" "}
+                {"Wooden Floors"}{" "}
+              </Link>
+            </li>
             <li className={styles.footer_item_list}>
               <Link href="/product/plywood">{"Plywood"}</Link>
             </li>
@@ -111,10 +117,12 @@ const Page = () => {
         </div>
       </div>
       <div>
-        <ScrollToTop smooth
-        viewBox="0 0 24 22" 
-        svgPath="m18.787 9.473s-4.505-4.502-6.259-6.255c-.147-.146-.339-.22-.53-.22-.192 0-.384.074-.531.22-1.753 1.753-6.256 6.252-6.256 6.252-.147.147-.219.339-.217.532.001.19.075.38.221.525.292.293.766.295 1.056.004l4.977-4.976v14.692c0 .414.336.75.75.75.413 0 .75-.336.75-.75v-14.692l4.978 4.978c.289.29.762.287 1.055-.006.145-.145.219-.335.221-.525.002-.192-.07-.384-.215-.529z"
-         color="#C3A464"/>
+        <ScrollToTop
+          smooth
+          viewBox="0 0 24 22"
+          svgPath="m18.787 9.473s-4.505-4.502-6.259-6.255c-.147-.146-.339-.22-.53-.22-.192 0-.384.074-.531.22-1.753 1.753-6.256 6.252-6.256 6.252-.147.147-.219.339-.217.532.001.19.075.38.221.525.292.293.766.295 1.056.004l4.977-4.976v14.692c0 .414.336.75.75.75.413 0 .75-.336.75-.75v-14.692l4.978 4.978c.289.29.762.287 1.055-.006.145-.145.219-.335.221-.525.002-.192-.07-.384-.215-.529z"
+          color="#C3A464"
+        />
       </div>
     </div>
   );
