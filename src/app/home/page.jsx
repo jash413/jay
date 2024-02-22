77ff1e1366b666fbdc1b28e384d9c37e264ba8fb
meetchauhandweck <meet.chauhan@dweckenterprise.com>
2024-02-22 18:24:51 +0530

    loader updated -m

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import HeroSection from "@/components/heroSection/page";
 import Timeline from "@/components/timeline/page";
 import Timeline1 from "@/components/timeline/page2";
@@ -15,7 +15,8 @@ import ScrollTrigger from "gsap/ScrollTrigger";
 import Lenis from "@studio-freight/lenis";
 import { useLayoutEffect } from "react";
 import "./homeParallax.css";
-const Page = () => {
+
+const Page = ({ lData }) => {
   useLayoutEffect(() => {
     const ctx = gsap.context(() => {
       // lenis integration for smooth scroll
@@ -44,29 +45,31 @@ const Page = () => {
     return () => ctx.revert();
   }, []);
   return (
-    <div>
+    <>
       <div>
-        <Navbar />
-      </div>
-      <section className="cards">
-        <div className="card">
-          <div className="card-cover">
-            <HeroSection
-              video_bg="./video/final_k.mp4"
-              homepage_heading="ply that is a living shield for beautiful and timeless furniture"
-            />
-          </div>
+        <div>
+          <Navbar />
         </div>
-        <Factory />
-      </section>
-      <Layers />
-      {/* <Plyspin /> */}
-      <Timeline />
-      <Timeline1 />
-      <HomeStore />
-      <Form />
-      <Footer />
-    </div>
+        <section className="cards">
+          <div className="card">
+            <div className="card-cover">
+              <HeroSection
+                video_bg="./video/final_k.mp4"
+                homepage_heading="ply that is a living shield for beautiful and timeless furniture"
+              />
+            </div>
+          </div>
+          <Factory loadImage={lData} />
+        </section>
+        <Layers loadLayer={lData} />
+        {/* <Plyspin /> */}
+        <Timeline loadUSP={lData} />
+        <Timeline1 />
+        <HomeStore />
+        <Form />
+        <Footer />
+      </div>
+    </>
   );
 };
 export default Page;
