667d17759dd700598ef6fdf90b5a4e118c36856b
Jay Kalariya <93344658+Jay-Kalariya@users.noreply.github.com>
2024-02-22 13:23:18 +0530

    upd -banner -j

--- src/app/Blogs/page.jsx
+++ src/app/Blogs/page.jsx
@@ -5,8 +5,9 @@ import { AnimatePresence } from "framer-motion";
 import Inner_header from "@/common/inner_header/page";
 import Preloader from "@/components/preloader/page";
 import Blogs_banner from "@/images/Blog_Banner.jpg";
+import Blogs_banner_mobile from "@/images/blogs_mobile.png";
 import BlogPost from "@/common/blogPosts/page";
-
+import styles from "@/app/Blogs/blogs.module.css"
 const Blogs = () => {
   // document.title="Blogs - Royale Touch Performance Ply";
   const [isLoading, setIsLoading] = useState(true);
@@ -31,8 +32,12 @@ const Blogs = () => {
       <AnimatePresence mode="wait">
         {isLoading && <Preloader />}
       </AnimatePresence>
-
+    <div className={styles.banner_img_desktop}>
       <Inner_header inner_header_image={Blogs_banner} heading_big="BLOGS" />
+      </div>
+      <div className={styles.blogs_banner_img}>
+      <Inner_header inner_header_image={Blogs_banner_mobile} heading_big="BLOGS" />
+      </div>
       <BlogPost />
       <Footer />
     </main>
