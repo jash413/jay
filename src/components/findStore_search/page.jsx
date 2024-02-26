133fd8dd17ab5c3eafe7bcecbbf398c4b906556e
utkarshyadav11 <utkarshyadav6745@gmail.com>
2024-02-26 13:17:25 +0530

    neo 2

--- src/components/findStore_search/page.jsx
+++ src/components/findStore_search/page.jsx
@@ -1292,7 +1292,7 @@ function Mapsection() {
         </div>
         <div className={styles.content70}>
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3671.425164494458!2d72.5118402758819!3d23.044869315494672!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e9b51fe5be8d9%3A0x1c4999478aa4081a!2sRoyale%20Touche%20Laminates%20and%20Wooden%20Floors%20-%20Ahmedabad!5e0!3m2!1sen!2sin!4v1699507206577!5m2!1sen!2sin"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3671.802628858455!2d72.46987617617147!3d23.03101801601143!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e9b09932f1e99%3A0x75fbb34492ff4f3e!2sRoyale%20Touche%20Wooden%20Floors!5e0!3m2!1sen!2sin!4v1708933279442!5m2!1sen!2sin"
             width="100%"
             height="720"
             allowfullscreen=""
