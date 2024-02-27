9aac0f0c146e3b51c85f51d5113b817d98431feb
Jay Kalariya <93344658+Jay-Kalariya@users.noreply.github.com>
2024-02-27 11:03:20 +0530

    upd

--- src/components/schemas/page.jsx
+++ src/components/schemas/page.jsx
@@ -4,8 +4,8 @@ export const ContactFormSchemas = Yup.object({
   Email: Yup.string().email('Invalid email address').required('Email is required'),
   PhoneNo: Yup.string()
   .matches(/^[0-9]+$/, 'Must be only digits')
-  .min(10, 'Must be exactly 10 digits')
-  .max(10, 'Must be exactly 10 digits')
+  .min(10, 'Phone number must be of 10 digits')
+  .max(10, 'Phone number must be of 10 digits')
   .required('Phone number is required'),
   // state: Yup.object().required("please select any one of the state"),
   City: Yup.object().required("please select any one of the city"),
